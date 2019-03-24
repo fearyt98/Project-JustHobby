7a5914a7dace72f479c53aca39d0ab6fe5fc8e0b
fearyt98 <leontievt@mail.ru>
2019-03-24 13:56:17 +0700

    Update

--- app/src/main/java/com/aurimteam/justhobby/MainActivity.kt
+++ app/src/main/java/com/aurimteam/justhobby/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
     fun clickEnter(view: View){
+        var intent: Intent = Intent(this, Registry::class.java)
+        startActivity(intent)
     }
-
 }
