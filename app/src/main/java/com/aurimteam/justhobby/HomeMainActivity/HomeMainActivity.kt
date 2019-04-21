09a975987001512f5af7807076b92707e0fb72f0
deonoize <yuyukin98@inbox.ru>
2019-04-21 12:16:07 +0700

    Update

--- app/src/main/java/com/aurimteam/justhobby/HomeMainActivity/HomeMainActivity.kt
+++ app/src/main/java/com/aurimteam/justhobby/HomeMainActivity/HomeMainActivity.kt
@@ -1,33 +1,28 @@
 package com.aurimteam.justhobby.HomeMainActivity
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import com.aurimteam.justhobby.R
 import kotlinx.android.synthetic.main.activity_home_main.*
+import java.util.*
+import java.text.SimpleDateFormat
+
 
 class HomeMainActivity : AppCompatActivity(), IView {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-
-
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_search -> {
-
-
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-
-
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_menu -> {
-
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -37,7 +32,13 @@ class HomeMainActivity : AppCompatActivity(), IView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_main)
-
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val df = SimpleDateFormat("d MMMM, EEEE")
+        val currentDateTimeString = df.format(Date())
+        homeCalendarText.text = currentDateTimeString
     }
 }
