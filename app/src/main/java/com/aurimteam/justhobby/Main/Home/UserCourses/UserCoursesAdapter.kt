e5f5c7885c84a60ba9ca8d8c3dd8b0e3ee640aab
deonoize <yuyukin98@inbox.ru>
2019-05-21 15:47:51 +0700

    Update

--- app/src/main/java/com/aurimteam/justhobby/Main/Home/UserCourses/UserCoursesAdapter.kt
+++ app/src/main/java/com/aurimteam/justhobby/Main/Home/UserCourses/UserCoursesAdapter.kt
@@ -2,30 +2,40 @@ package com.aurimteam.justhobby.Main.Home.UserCourses
 
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import com.aurimteam.justhobby.R
 import com.aurimteam.justhobby.Response.UserCourseResponse
-import com.aurimteam.justhobby.Response.UserCourseTimetableResponse
 import kotlinx.android.synthetic.main.fragment_card_user_course.view.*
+
 
 class UserCoursesAdapter : RecyclerView.Adapter<UserCoursesHolder>() {
 
-    private var isClicked = false
     private val userCoursesList: MutableList<UserCourseResponse> = mutableListOf()
+    private val isClickedList: MutableList<Boolean> = mutableListOf()
     override fun getItemCount(): Int = userCoursesList.size
     override fun onBindViewHolder(holder: UserCoursesHolder, position: Int) {
         holder.bind(
             userCoursesList[position].title,
             userCoursesList[position].description,
             userCoursesList[position].address,
-            userCoursesList[position].tutor
+            userCoursesList[position].tutor,
+            userCoursesList[position].timeTable,
+            isClickedList[position]
         )
-        showFirstDay(userCoursesList[position].timeTable[0], holder)
         holder.itemView.daysOfWeek.setOnClickListener {
-            if (!isClicked) showAllDays(position, holder)
-            else hideAllDays(position, holder)
+            if (!isClickedList[position]) {
+                setIsClicked(position, true)
+                notifyItemChanged(position)
+            } else {
+                setIsClicked(position, false)
+                notifyItemChanged(position)
+            }
         }
+    }
+
+    private fun setIsClicked(position: Int, value: Boolean) {
+        this.isClickedList[position] = value
+        notifyItemChanged(position)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, p1: Int): UserCoursesHolder =
@@ -36,110 +46,9 @@ class UserCoursesAdapter : RecyclerView.Adapter<UserCoursesHolder>() {
     fun onDataChange(userCourses: List<UserCourseResponse>) {
         this.userCoursesList.clear()
         this.userCoursesList.addAll(userCourses)
+        for (item in this.userCoursesList) {
+            isClickedList.add(this.userCoursesList.indexOf(item), false)
+        }
         notifyDataSetChanged()
-    }
-
-    private fun showAllDays(position: Int, holder: UserCoursesHolder) {
-        isClicked = true
-        holder.itemView.mondayWeek.visibility = View.GONE
-        holder.itemView.tuesdayWeek.visibility = View.GONE
-        holder.itemView.wednesdayWeek.visibility = View.GONE
-        holder.itemView.thursdayWeek.visibility = View.GONE
-        holder.itemView.fridayWeek.visibility = View.GONE
-        holder.itemView.saturdayWeek.visibility = View.GONE
-        holder.itemView.sundayWeek.visibility = View.GONE
-        for (item in userCoursesList[position].timeTable) {
-            when (item.dayWeek) {
-                "ПН" -> {
-                    holder.itemView.mondayStartTime.text = item.timeStart
-                    holder.itemView.mondayEndTime.text = item.timeEnd
-                    holder.itemView.mondayWeek.visibility = View.VISIBLE
-                }
-                "ВТ" -> {
-                    holder.itemView.tuesdayStartTime.text = item.timeStart
-                    holder.itemView.tuesdayEndTime.text = item.timeEnd
-                    holder.itemView.tuesdayWeek.visibility = View.VISIBLE
-                }
-                "СР" -> {
-                    holder.itemView.wednesdayStartTime.text = item.timeStart
-                    holder.itemView.wednesdayEndTime.text = item.timeEnd
-                    holder.itemView.wednesdayWeek.visibility = View.VISIBLE
-                }
-                "ЧТ" -> {
-                    holder.itemView.thursdayStartTime.text = item.timeStart
-                    holder.itemView.thursdayEndTime.text = item.timeEnd
-                    holder.itemView.thursdayWeek.visibility = View.VISIBLE
-                }
-                "ПТ" -> {
-                    holder.itemView.fridayStartTime.text = item.timeStart
-                    holder.itemView.fridayEndTime.text = item.timeEnd
-                    holder.itemView.fridayWeek.visibility = View.VISIBLE
-                }
-                "СБ" -> {
-                    holder.itemView.saturdayStartTime.text = item.timeStart
-                    holder.itemView.saturdayEndTime.text = item.timeEnd
-                    holder.itemView.saturdayWeek.visibility = View.VISIBLE
-                }
-                "ВС" -> {
-                    holder.itemView.sundayStartTime.text = item.timeStart
-                    holder.itemView.sundayEndTime.text = item.timeEnd
-                    holder.itemView.sundayWeek.visibility = View.VISIBLE
-                }
-            }
-        }
-        notifyItemChanged(position)
-    }
-
-    private fun hideAllDays(position: Int, holder: UserCoursesHolder) {
-        isClicked = false
-        holder.itemView.mondayWeek.visibility = View.GONE
-        holder.itemView.tuesdayWeek.visibility = View.GONE
-        holder.itemView.wednesdayWeek.visibility = View.GONE
-        holder.itemView.thursdayWeek.visibility = View.GONE
-        holder.itemView.fridayWeek.visibility = View.GONE
-        holder.itemView.saturdayWeek.visibility = View.GONE
-        holder.itemView.sundayWeek.visibility = View.GONE
-        showFirstDay(userCoursesList[position].timeTable[0], holder)
-        notifyItemChanged(position)
-    }
-
-    private fun showFirstDay(day: UserCourseTimetableResponse, holder: UserCoursesHolder) {
-        when (day.dayWeek) {
-            "ПН" -> {
-                holder.itemView.mondayStartTime.text = day.timeStart
-                holder.itemView.mondayEndTime.text = day.timeEnd
-                holder.itemView.mondayWeek.visibility = View.VISIBLE
-            }
-            "ВТ" -> {
-                holder.itemView.tuesdayStartTime.text = day.timeStart
-                holder.itemView.tuesdayEndTime.text = day.timeEnd
-                holder.itemView.tuesdayWeek.visibility = View.VISIBLE
-            }
-            "СР" -> {
-                holder.itemView.wednesdayStartTime.text = day.timeStart
-                holder.itemView.wednesdayEndTime.text = day.timeEnd
-                holder.itemView.wednesdayWeek.visibility = View.VISIBLE
-            }
-            "ЧТ" -> {
-                holder.itemView.thursdayStartTime.text = day.timeStart
-                holder.itemView.thursdayEndTime.text = day.timeEnd
-                holder.itemView.thursdayWeek.visibility = View.VISIBLE
-            }
-            "ПТ" -> {
-                holder.itemView.fridayStartTime.text = day.timeStart
-                holder.itemView.fridayEndTime.text = day.timeEnd
-                holder.itemView.fridayWeek.visibility = View.VISIBLE
-            }
-            "СБ" -> {
-                holder.itemView.saturdayStartTime.text = day.timeStart
-                holder.itemView.saturdayEndTime.text = day.timeEnd
-                holder.itemView.saturdayWeek.visibility = View.VISIBLE
-            }
-            "ВС" -> {
-                holder.itemView.sundayStartTime.text = day.timeStart
-                holder.itemView.sundayEndTime.text = day.timeEnd
-                holder.itemView.sundayWeek.visibility = View.VISIBLE
-            }
-        }
     }
 }
--- app/src/main/java/com/aurimteam/justhobby/Main/Home/UserCourses/UserCoursesHolder.kt
+++ app/src/main/java/com/aurimteam/justhobby/Main/Home/UserCourses/UserCoursesHolder.kt
@@ -10,11 +10,106 @@ class UserCoursesHolder(view: View) : RecyclerView.ViewHolder(view) {
         title: String,
         description: String,
         address: String,
-        tutor: String
+        tutor: String,
+        timeTable: List<UserCourseTimetableResponse>,
+        isClicked: Boolean
     ) {
         itemView.userCoursesTitle.text = title
         itemView.userCoursesDescription.text = description
         itemView.userCoursesAddress.text = address
         itemView.userCoursesTutor.text = tutor
+        if(isClicked) {
+            itemView.mondayWeek.visibility = View.GONE
+            itemView.tuesdayWeek.visibility = View.GONE
+            itemView.wednesdayWeek.visibility = View.GONE
+            itemView.thursdayWeek.visibility = View.GONE
+            itemView.fridayWeek.visibility = View.GONE
+            itemView.saturdayWeek.visibility = View.GONE
+            itemView.sundayWeek.visibility = View.GONE
+            for (item in timeTable) {
+                when (item.dayWeek) {
+                    "ПН" -> {
+                        itemView.mondayStartTime.text = item.timeStart
+                        itemView.mondayEndTime.text = item.timeEnd
+                        itemView.mondayWeek.visibility = View.VISIBLE
+                    }
+                    "ВТ" -> {
+                        itemView.tuesdayStartTime.text = item.timeStart
+                        itemView.tuesdayEndTime.text = item.timeEnd
+                        itemView.tuesdayWeek.visibility = View.VISIBLE
+                    }
+                    "СР" -> {
+                        itemView.wednesdayStartTime.text = item.timeStart
+                        itemView.wednesdayEndTime.text = item.timeEnd
+                        itemView.wednesdayWeek.visibility = View.VISIBLE
+                    }
+                    "ЧТ" -> {
+                        itemView.thursdayStartTime.text = item.timeStart
+                        itemView.thursdayEndTime.text = item.timeEnd
+                        itemView.thursdayWeek.visibility = View.VISIBLE
+                    }
+                    "ПТ" -> {
+                        itemView.fridayStartTime.text = item.timeStart
+                        itemView.fridayEndTime.text = item.timeEnd
+                        itemView.fridayWeek.visibility = View.VISIBLE
+                    }
+                    "СБ" -> {
+                        itemView.saturdayStartTime.text = item.timeStart
+                        itemView.saturdayEndTime.text = item.timeEnd
+                        itemView.saturdayWeek.visibility = View.VISIBLE
+                    }
+                    "ВС" -> {
+                        itemView.sundayStartTime.text = item.timeStart
+                        itemView.sundayEndTime.text = item.timeEnd
+                        itemView.sundayWeek.visibility = View.VISIBLE
+                    }
+                }
+            }
+        } else {
+            itemView.mondayWeek.visibility = View.GONE
+            itemView.tuesdayWeek.visibility = View.GONE
+            itemView.wednesdayWeek.visibility = View.GONE
+            itemView.thursdayWeek.visibility = View.GONE
+            itemView.fridayWeek.visibility = View.GONE
+            itemView.saturdayWeek.visibility = View.GONE
+            itemView.sundayWeek.visibility = View.GONE
+            when (timeTable[0].dayWeek) {
+                "ПН" -> {
+                    itemView.mondayStartTime.text = timeTable[0].timeStart
+                    itemView.mondayEndTime.text = timeTable[0].timeEnd
+                    itemView.mondayWeek.visibility = View.VISIBLE
+                }
+                "ВТ" -> {
+                    itemView.tuesdayStartTime.text = timeTable[0].timeStart
+                    itemView.tuesdayEndTime.text = timeTable[0].timeEnd
+                    itemView.tuesdayWeek.visibility = View.VISIBLE
+                }
+                "СР" -> {
+                    itemView.wednesdayStartTime.text = timeTable[0].timeStart
+                    itemView.wednesdayEndTime.text = timeTable[0].timeEnd
+                    itemView.wednesdayWeek.visibility = View.VISIBLE
+                }
+                "ЧТ" -> {
+                    itemView.thursdayStartTime.text = timeTable[0].timeStart
+                    itemView.thursdayEndTime.text = timeTable[0].timeEnd
+                    itemView.thursdayWeek.visibility = View.VISIBLE
+                }
+                "ПТ" -> {
+                    itemView.fridayStartTime.text = timeTable[0].timeStart
+                    itemView.fridayEndTime.text = timeTable[0].timeEnd
+                    itemView.fridayWeek.visibility = View.VISIBLE
+                }
+                "СБ" -> {
+                    itemView.saturdayStartTime.text = timeTable[0].timeStart
+                    itemView.saturdayEndTime.text = timeTable[0].timeEnd
+                    itemView.saturdayWeek.visibility = View.VISIBLE
+                }
+                "ВС" -> {
+                    itemView.sundayStartTime.text = timeTable[0].timeStart
+                    itemView.sundayEndTime.text = timeTable[0].timeEnd
+                    itemView.sundayWeek.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 }
