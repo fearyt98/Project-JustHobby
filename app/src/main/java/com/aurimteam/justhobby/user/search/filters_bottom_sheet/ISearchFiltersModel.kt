31856dcb9e0bae59a2b791a87483cde98a51d0db
deonoize <yuyukin98@inbox.ru>
2019-06-10 02:03:01 +0700

    Update

--- app/src/main/java/com/aurimteam/justhobby/user/search/filters_bottom_sheet/ISearchFiltersModel.kt
+++
@@ -1,5 +0,0 @@
-package com.aurimteam.justhobby.user.search.filters_bottom_sheet
-
-interface ISearchFiltersModel {
-    fun sendChosenFiltersData(onFinishedListener: SearchFiltersModel.OnFinishedListener)
-}
--- app/src/main/java/com/aurimteam/justhobby/user/search/filters_bottom_sheet/ISearchFiltersView.kt
+++
@@ -1,4 +0,0 @@
-package com.aurimteam.justhobby.user.search.filters_bottom_sheet
-
-interface ISearchFiltersView {
-}
